3ffbdbd4625677593b17208c462590f6d870d714
Sherry McCarty <sherrydmccarty@gmail.com>
2020-06-24 12:03:30 -0400

    thanks Chris!

--- src/auth/auth-service.js
+++ src/auth/auth-service.js
@@ -4,7 +4,7 @@ const config = require('../config')
 
 const AuthService = {
   getUserWithUserName(db, user_name) {
-    return db('character-creater-api')
+    return db('character_creater_users')
       .where({ user_name })
       .first()
   },
