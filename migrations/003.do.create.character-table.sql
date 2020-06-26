59ec1f692129236cc29e53424741c4a1e98076f9
Sherry McCarty <sherrydmccarty@gmail.com>
2020-06-26 17:35:44 -0400

    new migration to include new featuers in table characters

---
+++ migrations/003.do.create.character-table.sql
@@ -0,0 +1,5 @@
+ALTER TABLE characters ADD COLUMN age TEXT;
+ALTER TABLE characters ADD COLUMN sex TEXT;
+ALTER TABLE characters ADD COLUMN motivation TEXT;
+ALTER TABLE characters ADD COLUMN fear TEXT;
+ALTER TABLE characters ADD COLUMN history TEXT;
---
+++ migrations/003.undo.create.character-table.sql
@@ -0,0 +1,5 @@
+ALTER TABLE characters DROP COLUMN history;
+ALTER TABLE characters DROP COLUMN fear;
+ALTER TABLE characters DROP COLUMN motivation;
+ALTER TABLE characters DROP COLUMN sex;
+ALTER TABLE characters DROP COLUMN age;
